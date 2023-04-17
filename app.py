3d1d802df920b618ce60cfd258136af7f20f9494
majikyouser <sahiagb@gmail.com>
2023-04-17 09:56:18 -0400

    Fixed runstatement and connected db to flask

--- app.py
+++ app.py
@@ -8,23 +8,28 @@ app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'flask'
+app.config['MYSQL_DB'] = 'cityjail'
  
 mysql = MySQL(app)
 
-def insertAlias(criminal_id, alias):
+def runStatement(statement):
     cursor = mysql.connection.cursor()
-    cursor.execute(''' INSERT INTO Aliases VALUES({criminal_id},{alias}) ''')
+    cursor.execute(statement)
+    results = cursor.fetchall()
     mysql.connection.commit()
     cursor.close()
+    return results
 
 
 
 # Default route
 @app.route("/")
 def index():
+    print(runStatement('''SELECT * FROM criminals'''))
     return render_template("index.html")
 
+
+# Add routes here:
     
 if __name__ == "__main__":
     app.run(debug=True)
